74220ffa05c0852025f43ae9b4f3f8455b1b826c
Marc PEREZ <marc.perez.pro@gmail.com>
2022-01-06 16:54:38 +0100

    bundle gem unmustache

---
+++ lib/unmustache.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "unmustache/version"
+
+module Unmustache
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/unmustache/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Unmustache
+  VERSION = "0.1.0"
+end
